6e9356fb4737742c4a34d1b58c73748061114d69
Jayden Lee <jaydeh.lee@gmail.com>
2024-03-17 13:23:10 -0700

    healthz handler & refactor

--- main.go
+++ main.go
@@ -9,9 +9,14 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.Handle("/assets/", http.FileServer(http.Dir(".")))
-	
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/assets/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader (http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	corsMux := middlewareCors(mux)
 	httpServer := http.Server{Addr: ":8080", Handler: corsMux}
 	log.Fatal(httpServer.ListenAndServe())
